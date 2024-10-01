aef3eb20d6b43351e66a53ab3b8e4517daad072e
Susan Nkere <susan.nkere02@gmail.com>
2024-10-01 10:18:45 +0100

    Initial Commit for Easy Portfolio Navigation.html

---
+++ Mainstack script.js
@@ -0,0 +1,43 @@
+// Social Media Links
+document.getElementById('instagram').addEventListener('click', function() {
+    window.open('https://www.instagram.com/biig.sue', '_blank');
+});
+
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/susan-nkere', '_blank');
+});
+
+document.getElementById('facebook').addEventListener('click', function() {
+    window.open('https://www.facebook.com/chidi-nkere', '_blank');
+});
+
+document.getElementById('tiktok').addEventListener('click', function() {
+    window.open('https://www.tiktok.com/@big.sue_', '_blank');
+});
+
+document.getElementById('twitter').addEventListener('click', function() {
+    window.open('https://www.tiktok.com/@Biigsue', '_blank');
+});
+
+
+// View Portfolio Button
+document.getElementById('view-portfolio-btn').addEventListener('click', function() {
+    window.open('https://www.example.com/store', '_blank');
+});
+
+// Service Buttons
+document.getElementById('masterclass').addEventListener('click', function() {
+    window.open('https://www.example.com/masterclass', '_blank');
+});
+
+document.getElementById('Product-session').addEventListener('click', function() {
+    window.open('https://www.example.com/beginner-session', '_blank');
+});
+
+document.getElementById('Excel-session').addEventListener('click', function() {
+    window.open('https://www.example.com/creator-session', '_blank');
+});
+
+document.getElementById('internship').addEventListener('click', function() {
+    window.open('https://www.example.com/internship', '_blank');
+});
