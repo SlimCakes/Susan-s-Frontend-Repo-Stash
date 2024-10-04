ed7e5f13815ecf5d3608b868ccf42638cdf71ce2
Susan Nkere <susan.nkere02@gmail.com>
2024-10-04 21:05:10 +0100

    commit for portfolio.html

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html when the root is accessed
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
+});
+
+// Route for the second HTML file (about.html)
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'portfolio-About.html'));
 });
 
 const PORT = process.env.PORT || 3000;
