2682901c5bfe6099f3265dd42bbca52f89bb0d32
Susan Nkere <susan.nkere02@gmail.com>
2024-10-26 08:30:37 +0100

    second commit for October 26th

--- server.js
+++ server.js
@@ -7,13 +7,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html when the root is accessed
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
+  res.sendFile(path.join(__dirname, 'public', 'Home.html'));
 });
 
 // Route for the second HTML file (about.html)
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'portfolio-About.html'));
+  res.sendFile(path.join(__dirname, 'public', 'About.html'));
 });
+
+// Route for the third HTML file (about.html)
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Work.html'));
+});
+
+// Route for the fourth HTML file (about.html)
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Traclet-Project.html'));
+});
+
+// Route for the fifth HTML file (about.html)
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', '525System.html'));
+});
+
+// Route for the sixth HTML file (about.html)
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Tanta.html'));
+});
+
+// Route for the seventh HTML file (about.html)
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'UBA.html'));
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
