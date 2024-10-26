4d765dce98be6431650d41b3c172f009a7bb1d71
Susan Nkere <susan.nkere02@gmail.com>
2024-10-26 09:14:55 +0100

    Fifth commit for October 26th

--- server.js
+++ server.js
@@ -16,29 +16,10 @@ app.get('/about', (req, res) => {
 });
 
 // Route for the third HTML file (about.html)
-app.get('/about', (req, res) => {
+app.get('/work', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Work.html'));
 });
 
-// Route for the fourth HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Traclet-Project.html'));
-});
-
-// Route for the fifth HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', '525System.html'));
-});
-
-// Route for the sixth HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Tanta.html'));
-});
-
-// Route for the seventh HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'UBA.html'));
-});
 
 
 const PORT = process.env.PORT || 3000;
