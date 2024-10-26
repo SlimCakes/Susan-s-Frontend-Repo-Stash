3d1ee86e81123f5a8c7d91de8b0c00b718d6fdb0
Susan Nkere <susan.nkere02@gmail.com>
2024-10-26 08:38:16 +0100

    Third commit for Ocyober 26th

--- server.js
+++ server.js
@@ -10,37 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Home.html'));
 });
 
-// Route for the second HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'About.html'));
-});
-
-// Route for the third HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Work.html'));
-});
-
-// Route for the fourth HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Traclet-Project.html'));
-});
-
-// Route for the fifth HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', '525System.html'));
-});
-
-// Route for the sixth HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Tanta.html'));
-});
-
-// Route for the seventh HTML file (about.html)
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'UBA.html'));
-});
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
